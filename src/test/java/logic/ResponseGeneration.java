732e080c0882b7dc49a90420a74c877ab27a3402
skalatski <searhei.kalatski@idt.net>
2021-03-18 17:46:47 +0300

    Revert "With changes done in accordance with remarks"
    This reverts commit f416e90f

--- src/test/java/logic/ResponseGeneration.java
+++ src/test/java/logic/ResponseGeneration.java
@@ -3,28 +3,26 @@ package logic;
 import entity.Base;
 import entity.Item;
 import io.restassured.response.Response;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
 
-public class AnswersRequestSender {
+
+import java.util.List;
+
+public class ResponseGeneration {
     private Response response;
 
-    public Response get(String site, String page, int pageSize, String order, String sort, String filter) {
+    public Response queryParameters(String site, String page, int pageSize, String order, String sort, String filter) {
         response = given().queryParam("site", site)
                 .queryParam("page", page).queryParam("pagesize", pageSize)
                 .queryParam("order", order)
                 .queryParam("sort", sort)
                 .queryParam("filter", filter).get("/answers");
+        ;
         return response;
     }
 
     public List<Item> getItemsList() {
-        if (response == null) {
-            return new ArrayList<>();
-        }
         return response.then().extract().as(Base.class).getItems();
     }
+
 }
